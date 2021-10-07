95e7ea8223b522074079974aafe963a2924c5016
Araa <79429172+LittleCutePM@users.noreply.github.com>
2021-10-08 02:27:55 +0800

    Add files via upload

--- src/LittleAraaCute/AntiToolbox/discord/DiscordManager.php
+++ src/LittleAraaCute/AntiToolbox/discord/DiscordManager.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace YaN\AntiToolBox\discord;
+
+use YaN\AntiToolBox\discord\task\DiscordPost;
+use pocketmine\Server;
+
+class DiscordManager{
+
+	public static function postWebhook(string $url, string $content, string $username, array $embed = []): void{
+		$data = [
+			"username" => $username,
+			"content" => $content
+		];
+		if(!empty($embed)){
+			$data["embeds"] = $embed;
+			unset($data["content"]);
+		}else{
+			$msg = $data["content"];
+			$msg = str_replace("@everyone", "(@)everyone", $msg);
+			$msg = str_replace("@here", "(@)here", $msg);
+			$data["content"] = $msg;
+		}
+		$con = json_encode($data);
+		$post = new DiscordPost("https://discordapp.com/api/webhooks/" . $url, $con);
+		Server::getInstance()->getAsyncPool()->submitTask($post);
+	}
+}
